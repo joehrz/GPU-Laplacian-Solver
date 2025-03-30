6ceb903bb2f1ee942ee890996c12e5df952cb967
Dxxc <hrzich-j@webmail.uwinnipeg.ca>
2025-03-30 14:09:25 -0500

    adjusted parameters

--- cpu/include/config.h
+++ cpu/include/config.h
@@ -4,12 +4,12 @@
 #define CONFIG_H
 
 // Maximum number of iterations for SOR methods
-constexpr int MAX_ITER = 10000;
+constexpr int MAX_ITER = 20000;
 
 // Relaxation factor for SOR methods
-constexpr double OMEGA = 1.5;
+constexpr double OMEGA = 1.92;
 
 // Tolerance for convergence
-constexpr double TOL = 1e-6;
+constexpr double TOL = 1e-5;
 
 #endif // CONFIG_H
